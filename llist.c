7210c7722c15f34ec07eb1023586a9316ddcd484
Jack Trades <trades@box.(none)>
2011-05-22 10:52:24 -0500

    Added basic pair constructors.

---
+++ llist.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Creation of a pair */
+
+typedef struct pair {
+  int car;
+  struct pair *cdr;
+} pair;
+
+
+pair *alloc_pair(void) {
+  pair *obj;
+
+  obj = malloc(sizeof(pair));
+  if (obj == NULL) {
+    printf("Out of memory\n");
+  }
+  return obj;
+}
+
+pair *cons(int car, pair *cdr) {
+  pair *obj;
+  
+  obj = alloc_pair();
+  obj->car = car;
+  obj->cdr = cdr;
+  return obj;
+}
+
+
+
+
+void main() {
+  pair *d = cons(42, NULL);
+  printf("\n%d\n", d->car);
+}
